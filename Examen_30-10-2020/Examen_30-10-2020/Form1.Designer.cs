805dfa389a3d5d61f621b60c516e819c69b747a6
Jesus Valido Zafra <jesuszafra22@gmail.com>
2020-10-30 09:40:33 +0100

    Formulario dar de alta creado

--- Examen_30-10-2020/Examen_30-10-2020/Form1.Designer.cs
+++ Examen_30-10-2020/Examen_30-10-2020/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_alta_usuario = new System.Windows.Forms.Button();
+            this.btn_baja_usuario = new System.Windows.Forms.Button();
+            this.btn_sacar_libro = new System.Windows.Forms.Button();
+            this.btn_devolver_libro = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btn_alta_usuario
+            // 
+            this.btn_alta_usuario.Location = new System.Drawing.Point(126, 92);
+            this.btn_alta_usuario.Name = "btn_alta_usuario";
+            this.btn_alta_usuario.Size = new System.Drawing.Size(86, 23);
+            this.btn_alta_usuario.TabIndex = 0;
+            this.btn_alta_usuario.Text = "Darse de Alta";
+            this.btn_alta_usuario.UseVisualStyleBackColor = true;
+            // 
+            // btn_baja_usuario
+            // 
+            this.btn_baja_usuario.Location = new System.Drawing.Point(326, 92);
+            this.btn_baja_usuario.Name = "btn_baja_usuario";
+            this.btn_baja_usuario.Size = new System.Drawing.Size(86, 23);
+            this.btn_baja_usuario.TabIndex = 1;
+            this.btn_baja_usuario.Text = "Darse de Baja";
+            this.btn_baja_usuario.UseVisualStyleBackColor = true;
+            // 
+            // btn_sacar_libro
+            // 
+            this.btn_sacar_libro.Location = new System.Drawing.Point(126, 157);
+            this.btn_sacar_libro.Name = "btn_sacar_libro";
+            this.btn_sacar_libro.Size = new System.Drawing.Size(86, 23);
+            this.btn_sacar_libro.TabIndex = 2;
+            this.btn_sacar_libro.Text = "Sacar Libro";
+            this.btn_sacar_libro.UseVisualStyleBackColor = true;
+            // 
+            // btn_devolver_libro
+            // 
+            this.btn_devolver_libro.Location = new System.Drawing.Point(326, 157);
+            this.btn_devolver_libro.Name = "btn_devolver_libro";
+            this.btn_devolver_libro.Size = new System.Drawing.Size(86, 23);
+            this.btn_devolver_libro.TabIndex = 3;
+            this.btn_devolver_libro.Text = "Devolver Libro";
+            this.btn_devolver_libro.UseVisualStyleBackColor = true;
             // 
             // form_inicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(557, 299);
+            this.Controls.Add(this.btn_devolver_libro);
+            this.Controls.Add(this.btn_sacar_libro);
+            this.Controls.Add(this.btn_baja_usuario);
+            this.Controls.Add(this.btn_alta_usuario);
             this.Name = "form_inicio";
             this.Text = "Inicio";
             this.ResumeLayout(false);
@@ -42,6 +86,11 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_alta_usuario;
+        private System.Windows.Forms.Button btn_baja_usuario;
+        private System.Windows.Forms.Button btn_sacar_libro;
+        private System.Windows.Forms.Button btn_devolver_libro;
     }
 }
 
