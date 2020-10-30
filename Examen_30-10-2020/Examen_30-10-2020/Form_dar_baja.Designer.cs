3cd315701416734dcad09a833581948010b27176
Jesus Valido Zafra <jesuszafra22@gmail.com>
2020-10-30 09:47:27 +0100

    Formulario dar de baja creado

---
+++ Examen_30-10-2020/Examen_30-10-2020/Form_dar_baja.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Examen_30_10_2020
+{
+    partial class Form_dar_baja
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbox_socios = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_aceptar_baja = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstbox_socios
+            // 
+            this.lstbox_socios.FormattingEnabled = true;
+            this.lstbox_socios.Location = new System.Drawing.Point(12, 35);
+            this.lstbox_socios.Name = "lstbox_socios";
+            this.lstbox_socios.Size = new System.Drawing.Size(219, 277);
+            this.lstbox_socios.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Socios:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(350, 147);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre/Apellidos:";
+            // 
+            // btn_aceptar_baja
+            // 
+            this.btn_aceptar_baja.Location = new System.Drawing.Point(483, 145);
+            this.btn_aceptar_baja.Name = "btn_aceptar_baja";
+            this.btn_aceptar_baja.Size = new System.Drawing.Size(75, 23);
+            this.btn_aceptar_baja.TabIndex = 4;
+            this.btn_aceptar_baja.Text = "Dar de Baja";
+            this.btn_aceptar_baja.UseVisualStyleBackColor = true;
+            // 
+            // Form_dar_baja
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 320);
+            this.Controls.Add(this.btn_aceptar_baja);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstbox_socios);
+            this.Name = "Form_dar_baja";
+            this.Text = "Darse de Baja";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbox_socios;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_aceptar_baja;
+    }
+}
---
+++ Examen_30-10-2020/Examen_30-10-2020/Form_dar_baja.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen_30_10_2020
+{
+    public partial class Form_dar_baja : Form
+    {
+        public Form_dar_baja()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
