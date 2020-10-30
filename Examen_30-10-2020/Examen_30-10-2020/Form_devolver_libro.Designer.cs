b5b1088e8eac581462de042eb0b9e7995b428058
Jesus Valido Zafra <jesuszafra22@gmail.com>
2020-10-30 13:29:55 +0100

    Examen completado y funcional entregado

--- Examen_30-10-2020/Examen_30-10-2020/Form_devolver_libro.Designer.cs
+++ Examen_30-10-2020/Examen_30-10-2020/Form_devolver_libro.Designer.cs
@@ -72,7 +72,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(646, 289);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_devolver_libros);
             this.Controls.Add(this.datagrid_socios);
