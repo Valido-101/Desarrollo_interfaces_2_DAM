0395c130cfa72f342dd9135337d2bdc2e1dc17b5
Jesus Valido Zafra <jesuszafra22@gmail.com>
2020-10-30 09:36:29 +0100

    Botones creados y 5 objetos de cada clase inicializados

--- Examen_30-10-2020/Examen_30-10-2020/Form1.cs
+++ Examen_30-10-2020/Examen_30-10-2020/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,23 @@ namespace Examen_30_10_2020
 {
     public partial class form_inicio : Form
     {
+
+        ArrayList socios = new ArrayList();
+        ArrayList libros = new ArrayList();
+
         public form_inicio()
         {
             InitializeComponent();
+            libros.Add(new Libro("1", "Martín Matín", "Es un Agente o Drácula"));
+            libros.Add(new Libro("2", "Las Increíbles Aventuras de Lisandro", "Benito Camela"));
+            libros.Add(new Libro("3", "Por qué España sigue molando", "Santiago Abascal"));
+            libros.Add(new Libro("4", "Cómo armar un misil en 657 sencillos pasos", "Kim Jong Un"));
+            libros.Add(new Libro("5", "La Vida de Un Borderline", "Amador Rivas"));
+            socios.Add(new Socio("Juan","Valido Vázquez",1,new ArrayList()));
+            socios.Add(new Socio("Fernando", "García Sánchez", 2, new ArrayList()));
+            socios.Add(new Socio("Francisco Javier", "Gómez Bru", 3, new ArrayList()));
+            socios.Add(new Socio("Pilar", "Zambrana Pérez", 4, new ArrayList()));
+            socios.Add(new Socio("María", "Martín Navarro", 5, new ArrayList()));
         }
     }
 }
