79bb0dfa358da82c11357a48224e76b260c40d02
Jesus Valido Zafra <jesuszafra22@gmail.com>
2020-11-11 10:54:53 +0100

    Creada práctica 5

---
+++ Practica_5/Practica_5/Sala.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_5
+{
+    class Sala
+    {
+        private string nombre_evento;
+        private string fecha;
+        private string hora;
+        private ArrayList asientos;
+
+        public Sala(string nombre_evento, string fecha, string hora)
+        {
+            this.nombre_evento = nombre_evento;
+            this.fecha = fecha;
+            this.hora = hora;
+            asientos = new ArrayList();
+        }
+
+        public string Nombre_evento { get => nombre_evento; set => nombre_evento = value; }
+        public string Fecha { get => fecha; set => fecha = value; }
+        public string Hora { get => hora; set => hora = value; }
+        public ArrayList Asientos { get => asientos; set => asientos = value; }
+
+        public void addAsiento(Asiento a)
+        {
+            asientos.Add(a);
+        }
+
+        override public String ToString()
+        {
+            return Nombre_evento;
+        }
+    }
+}
