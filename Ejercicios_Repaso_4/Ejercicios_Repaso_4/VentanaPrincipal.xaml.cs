0662d1484e46801215e8b0e5c1490b86a0f14b6c
Jesús Valido Zafra <jesuszafra22@gmail.com>
2021-02-09 13:11:47 +0100

    Ejercicio 4 hecho

---
+++ Ejercicios_Repaso_4/Ejercicios_Repaso_4/VentanaPrincipal.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicios_Repaso_4
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class VentanaPrincipal : Window
+    {
+
+        //Variable de control que indica dónde se guarda el texto de TextBox
+        private bool conmutarPulsado = false;
+
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConmutar_Click(object sender, RoutedEventArgs e)
+        {
+            if (conmutarPulsado)
+            {
+                conmutarPulsado = false;
+            }
+            else
+            {
+                conmutarPulsado = true;
+            }
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            switch (conmutarPulsado)
+            {
+                case false:
+
+                    lstbox1.Items.Add(txtboxInput.Text);
+
+                    break;
+                case true:
+
+                    combobox1.Items.Add(txtboxInput.Text);
+
+                    break;
+            }
+
+            txtboxInput.Clear();
+        }
+
+        public ListBox getListBox()
+        {
+            return lstbox1;
+        }
+
+        public ComboBox getComboBox()
+        {
+            return combobox1;
+        }
+
+        private void btnNuevoFormulario_Click(object sender, RoutedEventArgs e)
+        {
+            ArrayList listaLstBox = new ArrayList();
+            ArrayList listaComboBox = new ArrayList();
+
+            foreach (string item in combobox1.Items)
+            {
+                listaComboBox.Add(item);
+            }
+
+            foreach (string item in lstbox1.Items)
+            {
+                listaLstBox.Add(item);
+            }
+
+            VentanaSecundaria ventanaSecundaria = new VentanaSecundaria(this, listaComboBox, listaLstBox);
+            ventanaSecundaria.Show();
+        }
+    }
+}
---
+++ Ejercicios_Repaso_4/Ejercicios_Repaso_4/VentanaSecundaria.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ejercicios_Repaso_4
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaSecundaria.xaml
+    /// </summary>
+    public partial class VentanaSecundaria : Window
+    {
+
+        private bool conmutarPulsado = false;
+        private VentanaPrincipal ventanaAnterior;
+
+        public VentanaSecundaria()
+        {
+            InitializeComponent();
+        }
+
+        public VentanaSecundaria(VentanaPrincipal ventanaAnterior, ArrayList datoscb, ArrayList datoslb)
+        {
+            InitializeComponent();
+
+            this.ventanaAnterior = ventanaAnterior;
+
+            foreach (var item in datoscb)
+            {
+                lstbox1.Items.Add(item);
+            }
+
+            foreach (var item in datoslb)
+            {
+                combobox1.Items.Add(item);
+            }
+        }
+
+        private void btnConmutar_Click(object sender, RoutedEventArgs e)
+        {
+            if (conmutarPulsado)
+            {
+                conmutarPulsado = false;
+            }
+            else
+            {
+                conmutarPulsado = true;
+            }
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            switch (conmutarPulsado)
+            {
+                case false:
+
+                    lstbox1.Items.Add(txtboxInput.Text);
+
+                    break;
+                case true:
+
+                    combobox1.Items.Add(txtboxInput.Text);
+
+                    break;
+            }
+
+            txtboxInput.Clear();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            foreach (var item in combobox1.Items)
+            {
+                if (!ventanaAnterior.getListBox().Items.Contains(item))
+                {
+                    ventanaAnterior.getListBox().Items.Add(item);
+                }
+            }
+
+            foreach (var item in lstbox1.Items)
+            {
+                if (!ventanaAnterior.getComboBox().Items.Contains(item))
+                {
+                    ventanaAnterior.getComboBox().Items.Add(item);
+                }
+            }
+        }
+    }
+}
